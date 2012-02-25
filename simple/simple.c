65c4569ee9c5fb5a76c1cf00681df31d470a1437
Pat Hickey <pat@moreproductive.org>
2012-02-24 21:32:28 -0800

    simple: fib on uint32t's

--- simple/simple.c
+++ simple/simple.c
@@ -3,24 +3,24 @@
 
 #include <utility/trace.h>
 
-uint16_t fib(uint16_t);
+uint32_t fib(uint32_t);
 
 void main (void) {
   
-  TRACE_CONFIGURE(TRACE_USART_0, 115200, BOARD_MCK);
+  TRACE_CONFIGURE(TRACE_DBGU, 115200, BOARD_MCK);
   TRACE_INFO("hello world\n\n\n");
 
   while (1) {
-    for (uint16_t i = 0; i < 20; i++) {
-      TRACE_INFO("fib %d = %d\n",i, fib(i));
+    for (uint32_t i = 0; i < 48; i++) {
+      TRACE_INFO("fib %ld = %ld\n",i, fib(i));
     }
   }
 }
 
 
-uint16_t fib (uint16_t n) {
+uint32_t fib (uint32_t n) {
   if (n == 0) return 0;
   if (n == 1) return 1;
-  return fib(n-1) + fib(n-2);
+  return fib(n-1) + fib(n-2); // Just to abuse the stack a bit.
 }
 
