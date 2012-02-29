8d413c36f863751082c0c18cf96f3308d24a633b
Pat Hickey <pat@moreproductive.org>
2012-02-28 23:27:33 -0800

    Add rtos-simple example application to build.
    * Nice feature of building at91lib board_cstartup_gnu.o into library:
      Include of target-specific object file containing those symbols will
      override them.
    * Builds, but not tested on board yet

---
+++ rtos-simple/rtos_simple.c
@@ -0,0 +1,39 @@
+
+#include <stdint.h>
+
+#include <utility/trace.h>
+
+uint32_t fib(uint32_t);
+
+void main (void) {
+  
+  TRACE_CONFIGURE(TRACE_DBGU, 115200, BOARD_MCK);
+  TRACE_INFO("hello world\n\n\n");
+
+  while (1) {
+    for (uint32_t i = 0; i < 48; i++) {
+      TRACE_INFO("fib %ld = %ld\n",i, fib(i));
+    }
+  }
+}
+
+
+uint32_t fib (uint32_t n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fib(n-1) + fib(n-2); // Just to abuse the stack a bit.
+}
+
+
+/* FreeRTOS Callbacks */
+
+void vApplicationTickHook(void) {
+  static int i = 0;
+  if (i < 1000) i++;
+  else { TRACE_INFO("tick\n"); i = 0; }
+}
+
+void vApplicationStackOverflowHook(void) {
+  TRACE_ERROR("Stack Overflow!\n")
+  for(;;);
+}
