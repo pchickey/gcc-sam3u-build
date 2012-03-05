59b106af9d20bbacc4237a1a3c7ce1f1c6110bf5
Pat Hickey <pat@moreproductive.org>
2012-03-04 16:11:47 -0800

    at91lib peripherals: add rest of library files for sam3u

--- at91lib/tgt.mk
+++ at91lib/tgt.mk
@@ -11,11 +11,33 @@ at91lib_board_cflags := -I$(AT91LIB)/boards/$(BOARD) \
 												
 
 at91lib_peripherals_path := $(AT91LIB)/peripherals
-at91lib_peripherals_objs := pio/pio.o \
+at91lib_peripherals_objs := adc/adc.o \
+                            adc/adc12.o \
+                            chipid/chipid.o \
                             dbgu/dbgu.o \
+                            dma/dma.o \
+                            eefc/eefc.o \
+                            hsmc4/hsmc4.o \
+                            hsmc4/hsmc4_ecc.o \
+                            irq/nvic.o \
+                            lcd/lcd.o \
+                            mci/mci.o \
+                            pio/pio.o \
+                            pio/pio_it.o \
+                            pwmc/pwmc2.o \
+                            rstc/rstc.o \
+                            rtc/rtc.o \
+                            rtt/rtt.o \
+                            slck/slck.o \
+                            spi/spi.o \
+                            systick/systick.o \
+                            tc/tc.o \
+                            tsadcc/tsadcc.o \
+                            twi/twi.o \
                             usart/usart.o
 at91lib_peripherals_cflags := -I$(AT91LIB)/boards/$(BOARD) \
 															-I$(AT91LIB)/peripherals \
+															-I$(TOP) \
 															-I$(AT91LIB)
 
 at91lib_utility_path := $(AT91LIB)
