9e0d3b739b4c22864514c2cb250b354b316f273c
Pat Hickey <pat@moreproductive.org>
2012-03-20 21:26:31 -0700

    rtos-ex-simple: add rtos_heap.c to build
    * aliases PortMalloc and PortFree to newlib malloc, free

---
+++ rtos-ex-simple/rtos_heap.c
@@ -0,0 +1,12 @@
+
+#include "stddef.h"
+
+void *pvPortMalloc(size_t s) {
+  return malloc(s); 
+}
+
+
+void vPortFree(void *p) {
+  return free(p);
+}
+
--- rtos-ex-simple/rtos_simple.c
+++ rtos-ex-simple/rtos_simple.c
@@ -3,27 +3,61 @@
 
 #include <utility/trace.h>
 
-uint32_t fib(uint32_t);
+#include <FreeRTOS.h>
+#include <task.h>
 
-void main (void) {
+void * fib_task_handle;
+void fib_task_func(void *);
+
+void * rad_task_handle;
+void rad_task_func(void *);
+
+int main (void) {
   
   TRACE_CONFIGURE(TRACE_DBGU, 115200, BOARD_MCK);
-  TRACE_INFO("hello world\n\n\n");
+  TRACE_INFO("entered main\n\n\n");
+  
+  xTaskCreate(fib_task_func, (signed portCHAR *)"fibt", 400,
+              NULL, 1, &fib_task_handle);
 
-  while (1) {
-    for (uint32_t i = 0; i < 48; i++) {
-      TRACE_INFO("fib %ld = %ld\n",i, fib(i));
-    }
-  }
+  xTaskCreate(rad_task_func, (signed portCHAR *)"radt", 400,
+              NULL, 1, &rad_task_handle);
+
+  vTaskStartScheduler();
 }
 
 
-uint32_t fib (uint32_t n) {
+uint16_t fib (uint16_t n) {
   if (n == 0) return 0;
   if (n == 1) return 1;
   return fib(n-1) + fib(n-2); // Just to abuse the stack a bit.
 }
 
+uint16_t rad (uint16_t n) {
+  if (n == 0) return 1;
+  if (n == 1) return 1;
+  return n * rad(n-1); // Just to abuse the stack a bit.
+}
+
+void fib_task_func (void * args) {
+  TRACE_INFO("fib task started\n");
+  while (1) {
+    for (uint16_t i = 0; i < 20; i++) {
+      TRACE_INFO("fib %d = %d\n",i, fib(i));
+      taskYIELD();
+    }
+  }
+}
+
+void rad_task_func (void * args) {
+  TRACE_INFO("rad task started\n");
+  while (1) {
+    for (uint16_t i = 0; i < 10; i++) {
+      TRACE_INFO("rad %d = %d\n",i, rad(i));
+      taskYIELD();
+    }
+  }
+}
 
 /* FreeRTOS Callbacks */
 
